b6a155fa62a170ba1898ca97a1f247acbdedfb71
hoosain <hoosain.madhi@gmail.com>
2021-10-18 11:02:52 +0200

    AddTask

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,7 @@
 <template>
 <div class="container">
     <Header title="Task Tracker" />
+    <AddTask @add-task="addTask"/>
     <Tasks @toggle-reminder="toggleReminder" 
     @delete-task="deleteTask" :tasks="tasks" />
 </div>
@@ -9,12 +10,14 @@
 <script>
 import Header  from './components/Header'
 import Tasks from './components/Tasks'
+import AddTask from './components/AddTask'
 
 export default {
   name: 'App',
   components: {
     Header,
-    Tasks
+    Tasks,
+    AddTask,
   },
   data() {
     return{
@@ -30,6 +33,10 @@ export default {
       task.id===id? {...task, 
       reminder: !task.reminder}: task)
     },
+    addTask(task){
+      this.tasks=[...this.tasks,task]
+
+    }
   },
   created(){//lifecyle method
     this.tasks = [
